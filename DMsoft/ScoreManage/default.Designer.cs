4818d465371fe0477d684856563eb0f76678efd9
JamesVinci <441394194@qq.com>
2019-07-11 16:51:20 +0800

    新增项目
    新增分数管理项目

---
+++ DMsoft/ScoreManage/default.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace ScoreManage
+{
+    partial class defaultFrom
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // defaultFrom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(792, 573);
+            this.Font = new System.Drawing.Font("宋体", 12F);
+            this.Name = "defaultFrom";
+            this.Text = "Score Management";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
