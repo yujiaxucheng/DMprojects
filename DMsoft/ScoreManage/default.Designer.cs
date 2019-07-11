919a233278f9bda5b33cfe0b1845518647294177
JamesVinci <441394194@qq.com>
2019-07-11 17:20:20 +0800

    增加图表控件
    分数管理项目，新增图表控件

--- DMsoft/ScoreManage/default.Designer.cs
+++ DMsoft/ScoreManage/default.Designer.cs
@@ -28,21 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.pbMain = new System.Windows.Forms.PictureBox();
+            this.C1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.C2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.C3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.C4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.C5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.C6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgvData
+            // 
+            this.dgvData.AllowUserToAddRows = false;
+            this.dgvData.AllowUserToDeleteRows = false;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.C1,
+            this.C2,
+            this.C3,
+            this.C4,
+            this.C5,
+            this.C6});
+            this.dgvData.Location = new System.Drawing.Point(93, 58);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.ReadOnly = true;
+            this.dgvData.RowHeadersVisible = false;
+            this.dgvData.RowTemplate.Height = 23;
+            this.dgvData.Size = new System.Drawing.Size(604, 150);
+            this.dgvData.TabIndex = 0;
+            // 
+            // pbMain
+            // 
+            this.pbMain.Location = new System.Drawing.Point(45, 240);
+            this.pbMain.Name = "pbMain";
+            this.pbMain.Size = new System.Drawing.Size(700, 300);
+            this.pbMain.TabIndex = 1;
+            this.pbMain.TabStop = false;
+            // 
+            // C1
+            // 
+            this.C1.HeaderText = "日期";
+            this.C1.Name = "C1";
+            this.C1.ReadOnly = true;
+            // 
+            // C2
+            // 
+            this.C2.HeaderText = "甲";
+            this.C2.Name = "C2";
+            this.C2.ReadOnly = true;
+            // 
+            // C3
+            // 
+            this.C3.HeaderText = "乙";
+            this.C3.Name = "C3";
+            this.C3.ReadOnly = true;
+            // 
+            // C4
+            // 
+            this.C4.HeaderText = "丙";
+            this.C4.Name = "C4";
+            this.C4.ReadOnly = true;
+            // 
+            // C5
+            // 
+            this.C5.HeaderText = "丁";
+            this.C5.Name = "C5";
+            this.C5.ReadOnly = true;
+            // 
+            // C6
+            // 
+            this.C6.HeaderText = "戊";
+            this.C6.Name = "C6";
+            this.C6.ReadOnly = true;
             // 
             // defaultFrom
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(792, 573);
+            this.Controls.Add(this.pbMain);
+            this.Controls.Add(this.dgvData);
             this.Font = new System.Drawing.Font("宋体", 12F);
             this.Name = "defaultFrom";
             this.Text = "Score Management";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C6;
+        private System.Windows.Forms.PictureBox pbMain;
     }
 }
 
