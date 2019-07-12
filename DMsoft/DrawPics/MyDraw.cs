bf0eebca6fa9da2b38ea0afc62824af4570adbf5
JamesVinci <441394194@qq.com>
2019-07-12 16:53:23 +0800

    圆角绘制
    实现阶梯钻头圆角绘制

--- DMsoft/DrawPics/MyDraw.cs
+++ DMsoft/DrawPics/MyDraw.cs
@@ -75,9 +75,11 @@ namespace DrawPics
 
             DrawCoor(pbMain.CreateGraphics(), p);
 
-            fPos = DrawDrill(pbMain.CreateGraphics(), p, D1, D2, L1, a1, 0f);
-            fPos = DrawDrill(pbMain.CreateGraphics(), p, D2, D2 + 100f, L1, a2, fPos);
-            DrawArc(pbMain.CreateGraphics(), p, pArc, R1, sa, swa);
+            fPos = DrawDrill(pbMain.CreateGraphics(), p, D1, D2, L1, L2, a1, R1, R1, 0f);
+
+            //fPos = DrawDrill(pbMain.CreateGraphics(), p, D1, D2, L1, a1, fPos);
+            //fPos = DrawDrill(pbMain.CreateGraphics(), p, D2, D2 + 100f, L1, a2, fPos);
+            //DrawArc(pbMain.CreateGraphics(), p, pArc, R1, sa, swa);
 
             this.tDraw.Enabled = false;
         }
@@ -97,14 +99,14 @@ namespace DrawPics
             this.tbM21.Text = "0";
             this.tbM22.Text = "-1";
 
-            this.tbL1.Text = "150";
-            this.tbD1.Text = "0";
-            this.tba1.Text = "140";
-            this.tbL2.Text = "150";
-            this.tbD2.Text = "150";
+            this.tbL1.Text = "180";
+            this.tbD1.Text = "90";
+            this.tba1.Text = "120";
+            this.tbL2.Text = "3000";
+            this.tbD2.Text = "200";
             this.tba2.Text = "90";
 
-            this.tbR1.Text = "100";
+            this.tbR1.Text = "94";
             this.tbArcX.Text = "-298";
             this.tbArcY.Text = "25";
             this.tbS.Text = "90";
@@ -167,24 +169,6 @@ namespace DrawPics
             g.DrawLine(p, p3.X, p3.Y, p4.X, p4.Y);
             g.DrawLine(p, p5.X, p5.Y, p6.X, p6.Y);
 
-            this.lPointInfo.Text = "";
-            PrintPoint(this.lPointInfo, p0, "p0");
-            PrintPoint(this.lPointInfo, p1, "p1");
-            PrintPoint(this.lPointInfo, p2, "p2");
-            PrintPoint(this.lPointInfo, p3, "p3");
-            PrintPoint(this.lPointInfo, p4, "p4");
-            PrintPoint(this.lPointInfo, p5, "p5");
-            PrintPoint(this.lPointInfo, p6, "p6");
-
-            #region 计算圆弧矩阵 坐标
-
-            Point tmp;
-            tmp.X = p3.X - (float)(R1 * Tan(a2 * 0.25) + R1);
-            tmp.Y = p3.Y;
-            PrintPoint(this.lPointInfo, tmp, "圆弧矩阵");
-
-            #endregion
-
             fPos = p5.X;
             return fPos;
         }
@@ -206,7 +190,8 @@ namespace DrawPics
         private float DrawDrill(Graphics g, Pen p, double D1, double D2, double L1, double L2, double a, double R1, double R2, float fPos)
         {
             Point p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12;            // 钻头图形上点
-            Point p_t1, p_t2, p_t3, p_t4;                                       // 辅助点位置
+            Point p_A1, p_A2, p_A3, p_A4;                                       // 圆弧辅助点
+            Point p_t1, p_t2, p_t3, p_t4;                                       // 钻头图辅助点
 
             g.Transform = new Matrix(m11, m12, m21, m22, p0.X, p0.Y);           // 设置矩阵
             p.Color = Color.Blue;                                               // 线颜色
@@ -233,25 +218,64 @@ namespace DrawPics
             p_t3.Y = +(float)(0.5 * D2);
             p_t4.Y = -(float)(0.5 * D2);
 
+            p9.X = p10.X = p_t3.X - (float)(R1 * Tan(0.25 * a));
+            p9.Y = +(float)(0.5 * D2);
+            p10.Y = -(float)(0.5 * D2);
 
+            p7.X = p8.X = p9.X + (float)(2d * R1 * Sin(0.25 * a) * Cos(0.25 * a));
+            p7.Y = p9.Y - (float)(2d * R1 * Sin(0.25 * a) * Sin(0.25 * a));
+            p8.Y = p10.Y + (float)(2d * R1 * Sin(0.25 * a) * Sin(0.25 * a));
 
+            p11.X = p12.X = p_t3.X - (float)L2;
+            p11.Y = +(float)(0.5 * D2);
+            p12.Y = -(float)(0.5 * D2);
 
+            // 钻头除圆角之外的连线
+            g.DrawLine(p, p1.X, p1.Y, p3.X, p3.Y);
+            g.DrawLine(p, p5.X, p5.Y, p7.X, p7.Y);
+            g.DrawLine(p, p9.X, p9.Y, p11.X, p11.Y);
 
-            //g.DrawLine(p, p1.X, p1.Y, p3.X, p3.Y);
-            //g.DrawLine(p, p5.X, p5.Y, p7.X, p7.Y);
-            //g.DrawLine(p, p9.X, p9.Y, p11.X, p11.Y);
-            //g.DrawLine(p, p2.X, p2.Y, p4.X, p4.Y);
-            //g.DrawLine(p, p6.X, p6.Y, p8.X, p8.Y);
-            //g.DrawLine(p, p10.X, p10.Y, p12.X, p12.Y);
+            g.DrawLine(p, p2.X, p2.Y, p4.X, p4.Y);
+            g.DrawLine(p, p6.X, p6.Y, p8.X, p8.Y);
+            g.DrawLine(p, p10.X, p10.Y, p12.X, p12.Y);
 
-            //g.DrawLine(p, p1.X, p1.Y, p2.X, p2.Y);
-            //g.DrawLine(p, p3.X, p3.Y, p4.X, p4.Y);
-            //g.DrawLine(p, p5.X, p5.Y, p6.X, p6.Y);
-            //g.DrawLine(p, p7.X, p7.Y, p8.X, p8.Y);
-            //g.DrawLine(p, p9.X, p9.Y, p10.X, p10.Y);
-            //g.DrawLine(p, p11.X, p11.Y, p12.X, p12.Y);
+            g.DrawLine(p, p1.X, p1.Y, p2.X, p2.Y);
+            g.DrawLine(p, p3.X, p3.Y, p4.X, p4.Y);
+            g.DrawLine(p, p5.X, p5.Y, p6.X, p6.Y);
+            g.DrawLine(p, p7.X, p7.Y, p8.X, p8.Y);
+            g.DrawLine(p, p9.X, p9.Y, p10.X, p10.Y);
+            g.DrawLine(p, p11.X, p11.Y, p12.X, p12.Y);
 
-            //fPos = p5.X;
+            // 圆弧辅助矩形左下顶点
+            p_A1.X = p3.X - (float)R1;
+            p_A1.Y = p3.Y;
+            p_A2.X = p4.X - (float)R1;
+            p_A2.Y = p4.Y - (float)(2d * R1);
+            p_A3.X = p9.X - (float)R2;
+            p_A3.Y = p9.Y - (float)(2d * R2);
+            p_A4.X = p10.X - (float)R2;
+            p_A4.Y = p10.Y;
+
+            DrawArc(g, p, p_A1, R1, -90, -(a * 0.5));
+            DrawArc(g, p, p_A2, R1, 90, (a * 0.5));
+            DrawArc(g, p, p_A3, R2, 90, -(a * 0.5));
+            DrawArc(g, p, p_A4, R2, -90, (a * 0.5));
+
+            this.lPointInfo.Text = "";
+            PrintPoint(this.lPointInfo, p1, "p1");
+            PrintPoint(this.lPointInfo, p2, "p2");
+            PrintPoint(this.lPointInfo, p3, "p3");
+            PrintPoint(this.lPointInfo, p4, "p4");
+            PrintPoint(this.lPointInfo, p5, "p5");
+            PrintPoint(this.lPointInfo, p6, "p6");
+            PrintPoint(this.lPointInfo, p7, "p7");
+            PrintPoint(this.lPointInfo, p8, "p8");
+            PrintPoint(this.lPointInfo, p9, "p9");
+            PrintPoint(this.lPointInfo, p10, "p10");
+            PrintPoint(this.lPointInfo, p11, "p11");
+            PrintPoint(this.lPointInfo, p12, "p12");
+
+            fPos = p11.X;
             return fPos;
         }
 
@@ -263,11 +287,9 @@ namespace DrawPics
         /// <param name="P">圆弧矩阵坐标</param>
         /// <param name="R">半径</param>
         /// <param name="start">起始角</param>
-        /// <param name="a">扫过角</param>
+        /// <param name="a">顺时针扫过角</param>
         private float DrawArc(Graphics g, Pen p, Point P, double R, double start, double a)
         {
-            //Point p1, p2, p3, p4;
-
             g.Transform = new Matrix(m11, m12, m21, m22, p0.X, p0.Y);       // 设置矩阵
             p.Color = Color.Blue;                                           // 线颜色
             p.DashStyle = DashStyle.Solid;                                  // 线型
