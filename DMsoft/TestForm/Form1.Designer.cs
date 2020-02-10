7b8f27de42d0919915dbcd0a9cec2dbc3bbb4571
yujiaxucheng <441394194@qq.com>
2020-02-10 18:25:42 +0800

    打开对话框
    获取打开的文件名和文件夹名

---
+++ DMsoft/TestForm/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace TestForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbFileOrFolderName = new System.Windows.Forms.TextBox();
+            this.btnChooseFolder = new System.Windows.Forms.Button();
+            this.tbFileOrFolderName2 = new System.Windows.Forms.TextBox();
+            this.btnChooseFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbFileOrFolderName
+            // 
+            this.tbFileOrFolderName.Font = new System.Drawing.Font("宋体", 12F);
+            this.tbFileOrFolderName.Location = new System.Drawing.Point(102, 83);
+            this.tbFileOrFolderName.Name = "tbFileOrFolderName";
+            this.tbFileOrFolderName.Size = new System.Drawing.Size(646, 30);
+            this.tbFileOrFolderName.TabIndex = 0;
+            // 
+            // btnChooseFolder
+            // 
+            this.btnChooseFolder.Font = new System.Drawing.Font("宋体", 12F);
+            this.btnChooseFolder.Location = new System.Drawing.Point(769, 83);
+            this.btnChooseFolder.Name = "btnChooseFolder";
+            this.btnChooseFolder.Size = new System.Drawing.Size(75, 30);
+            this.btnChooseFolder.TabIndex = 1;
+            this.btnChooseFolder.Text = "…";
+            this.btnChooseFolder.UseVisualStyleBackColor = true;
+            this.btnChooseFolder.Click += new System.EventHandler(this.btnChooseFolder_Click);
+            // 
+            // tbFileOrFolderName2
+            // 
+            this.tbFileOrFolderName2.Font = new System.Drawing.Font("宋体", 12F);
+            this.tbFileOrFolderName2.Location = new System.Drawing.Point(102, 156);
+            this.tbFileOrFolderName2.Name = "tbFileOrFolderName2";
+            this.tbFileOrFolderName2.Size = new System.Drawing.Size(646, 30);
+            this.tbFileOrFolderName2.TabIndex = 0;
+            // 
+            // btnChooseFile
+            // 
+            this.btnChooseFile.Font = new System.Drawing.Font("宋体", 12F);
+            this.btnChooseFile.Location = new System.Drawing.Point(769, 156);
+            this.btnChooseFile.Name = "btnChooseFile";
+            this.btnChooseFile.Size = new System.Drawing.Size(75, 30);
+            this.btnChooseFile.TabIndex = 1;
+            this.btnChooseFile.Text = "…";
+            this.btnChooseFile.UseVisualStyleBackColor = true;
+            this.btnChooseFile.Click += new System.EventHandler(this.btnChooseFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1030, 613);
+            this.Controls.Add(this.btnChooseFile);
+            this.Controls.Add(this.btnChooseFolder);
+            this.Controls.Add(this.tbFileOrFolderName2);
+            this.Controls.Add(this.tbFileOrFolderName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbFileOrFolderName;
+        private System.Windows.Forms.Button btnChooseFolder;
+        private System.Windows.Forms.TextBox tbFileOrFolderName2;
+        private System.Windows.Forms.Button btnChooseFile;
+    }
+}
+
---
+++ DMsoft/TestForm/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestForm
+{
+    public partial class Form1 : Form
+    {
+        private string fName = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region 代码参考出处
+
+        // 原文链接：https://blog.csdn.net/weixin_44490080/article/details/100712683
+
+        // 打开文件夹，并在文本框显示名称
+        private void btnChooseFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            folder.Description = "选择目录";
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                fName = folder.SelectedPath;
+            }
+            this.tbFileOrFolderName.Text = fName;
+        }
+
+        // 打开文件，并在文本框显示名称
+        private void btnChooseFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.Title = "请选择图片";
+            dialog.Multiselect = false;                 //是否支持多个文件的打开：否
+            dialog.Filter = "表格(*.xls)|*.xls";
+
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                fName = dialog.FileName;
+            }
+            this.tbFileOrFolderName.Text = fName;
+        }
+
+        #endregion
+    }
+}
